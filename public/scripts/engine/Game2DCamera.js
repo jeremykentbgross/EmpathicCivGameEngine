f7fce1e5e4fa14bbc1ae79b7d4f8670b017991fa
Jeremy Kent B Gross <jeremykentbgross@gmail.com>
2012-08-26 20:43:01 +0200

    Starting to get rid of namespace.createX(instance, private); 	modified:   public/scripts/engine/Game2DAABB.js 	modified:   public/scripts/engine/Game2DCamera.js

--- public/scripts/engine/Game2DCamera.js
+++ public/scripts/engine/Game2DCamera.js
@@ -1,13 +1,33 @@
+
+//TODO depricated!
 GameEngineLib.createGame2DCamera = function(instance, private)
 {
+	var temp = new GameEngineLib.Game2DCamera();
 	instance = instance || {};
-	private = private || {};
 	
-	//TODO debug add private
+	for(property in temp)
+	{
+		instance[property] = temp[property]
+	}
+	for(property in temp.prototype)
+	{
+		instance[property] = temp.prototype[property];
+	}
 	
+	return instance;
+}
+
+
+
+
+
+
+
+GameEngineLib.Game2DCamera = function Game2DCamera()
+{
 	if(!GameSystemVars.Network.isServer)
 	{
-		private.myRect = GameEngineLib.createGame2DAABB(
+		this._myRect = GameEngineLib.createGame2DAABB(
 			0,
 			0,
 			GameInstance.Graphics.getWidth(),
@@ -16,48 +36,54 @@ GameEngineLib.createGame2DCamera = function(instance, private)
 	}
 	else
 	{
-		private.myRect = GameEngineLib.createGame2DAABB(
+		this._myRect = GameEngineLib.createGame2DAABB(
 			0,
 			0,
 			GameSystemVars.Graphics.initWidth,
 			GameSystemVars.Graphics.initHeight
 		);
 	}
+}
+GameEngineLib.Game2DCamera.prototype.constructor = GameEngineLib.Game2DCamera;
+
+
+
+
+GameEngineLib.Game2DCamera.prototype.init = function init(inWidth, inHeight)
+{
+	if(inWidth &&inHeight)
+		this._myRect = GameEngineLib.createGame2DAABB(0, 0, inWidth, inHeight);
+}
+
+
+
+GameEngineLib.Game2DCamera.prototype.centerOn = function centerOn(inTargetCenter, inMap)
+{		
+	var camPoint = inTargetCenter.subtract(this._myRect.getWidthHeight().multiply(0.5));
 	
-	instance.init = function(inWidth, inHeight)
+	//if the map does not wrap, clamp camera to within the world favoring the uper left corner
+	if(inMap && inMap.isWrappable && !inMap.isWrappable())
 	{
-		if(inWidth &&inHeight)
-			private.myRect = GameEngineLib.createGame2DAABB(0, 0, inWidth, inHeight);
+		camPoint = camPoint.componentMin(inMap.getMapLowerRight().subtract(this._myRect.getWidthHeight()));
+		camPoint = camPoint.componentMax(GameEngineLib.createGame2DPoint(0,0));
 	}
 	
-	instance.centerOn = function(inTargetCenter, inMap)
-	{		
-		var camPoint = inTargetCenter.subtract(private.myRect.getWidthHeight().multiply(0.5));
-		
-		//if the map does not wrap, clamp camera to within the world favoring the uper left corner
-		if(inMap && inMap.isWrappable && !inMap.isWrappable())
-		{
-			camPoint = camPoint.componentMin(inMap.getMapLowerRight().subtract(private.myRect.getWidthHeight()));
-			camPoint = camPoint.componentMax(GameEngineLib.createGame2DPoint(0,0));
-		}
-		
-		private.myRect.setLeftTop(camPoint);
-	}
-	
-	//TODO have actual target entity?  I think so
-	instance.getTargetPosition = function()
-	{
-		return private.myRect.getCenter();
-	}
-	
-	//TODO listen to graphics object for resizes?
-	
-	//todo make this gameobject? with set target/map? then needs update.. eh?
-	
-	instance.getRect = function()
-	{
-		return private.myRect;
-	}
-	
-	return instance;
+	this._myRect.setLeftTop(camPoint);
+}
+
+
+
+//TODO have actual target entity?  I think so
+GameEngineLib.Game2DCamera.prototype.getTargetPosition = function getTargetPosition()
+{
+	return this._myRect.getCenter();
+}
+
+//TODO listen to graphics object for resizes?
+
+//todo make this gameobject? with set target/map? then needs update.. eh?
+
+GameEngineLib.Game2DCamera.prototype.getRect = function getRect()
+{
+	return this._myRect;
 }
