1621f31ed5acbcdd878b4467e5ffe093e0eb8f53
Jeremy Kent B Gross <jeremykentbgross@gmail.com>
2012-08-27 19:53:50 +0200

    Added Licence Information. 	modified:   .gitignore 	new file:   COPYING 	new file:   COPYING.LESSER 	new file:   LICENSE 	modified:   private/GameServer.js 	modified:   private/scripts/GameServerSideNetwork.js 	modified:   public/scripts/alife/GameRules.js 	modified:   public/scripts/engine/Game2DAABB.js 	modified:   public/scripts/engine/Game2DCamera.js 	modified:   public/scripts/engine/Game2DGraphics.js 	modified:   public/scripts/engine/Game2DMap.js 	modified:   public/scripts/engine/Game2DPhysics.js 	modified:   public/scripts/engine/Game2DPoint.js 	modified:   public/scripts/engine/Game2DSceneGraph.js 	modified:   public/scripts/engine/Game2DTileSet.js 	modified:   public/scripts/engine/Game2DWorld.js 	modified:   public/scripts/engine/GameArithmeticCompression.js 	modified:   public/scripts/engine/GameAssetManager.js 	modified:   public/scripts/engine/GameBinarySerializer.js 	modified:   public/scripts/engine/GameBitPacker.js 	modified:   public/scripts/engine/GameCircularDoublyLinkedListNode.js 	modified:   public/scripts/engine/GameClientSideNetwork.js 	modified:   public/scripts/engine/GameEngineLogger.js 	modified:   public/scripts/engine/GameEntity.js 	modified:   public/scripts/engine/GameEntityComponent_2DCamera.js 	modified:   public/scripts/engine/GameEntityComponent_2DPhysics.js 	modified:   public/scripts/engine/GameEntityComponent_Input.js 	modified:   public/scripts/engine/GameEntityComponent_Sprite.js 	modified:   public/scripts/engine/GameEventSystem.js 	modified:   public/scripts/engine/GameFrameWork.js 	modified:   public/scripts/engine/GameInput.js 	modified:   public/scripts/engine/GameObject.js 	modified:   public/scripts/engine/GameObjectClassFactory.js 	modified:   public/scripts/engine/GameQuadTree.js 	modified:   public/scripts/engine/GameRegistry.js 	modified:   public/scripts/engine/GameSpriteManager.js 	modified:   public/scripts/engine/GameSystemVars.js 	modified:   public/scripts/engine/GameTimer.js 	modified:   public/scripts/engine/unit_tests/TODO_TestMultPacker.js 	modified:   public/scripts/engine/unit_tests/TestGameArithmeticCompression.js 	modified:   public/scripts/engine/unit_tests/TestGameBinarySerializer.js 	modified:   public/scripts/engine/unit_tests/TestGameBitPacker.js 	modified:   public/scripts/engine/unit_tests/TestGameEventSystem.js 	modified:   public/scripts/engine/unit_tests/TestGameObjectClassSystem.js 	modified:   public/scripts/game/GameRules.js

--- public/scripts/engine/Game2DMap.js
+++ public/scripts/engine/Game2DMap.js
@@ -1,3 +1,24 @@
+/*
+© Copyright 2012 Jeremy Gross
+	jeremykentbgross@gmail.com
+	Distributed under the terms of the GNU Lesser GPL (LGPL)
+		
+	This file is part of EmpathicCivGameEngine™.
+	
+	EmpathicCivGameEngine™ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	EmpathicCivGameEngine™ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with EmpathicCivGameEngine™.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 GameEngineLib.createGame2DMap = function(instance, private)
 {
 	instance = instance || {};
