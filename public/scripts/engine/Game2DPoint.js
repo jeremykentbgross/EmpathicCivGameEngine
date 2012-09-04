7edd59106f59a61637886b5daa44ed0ac63db918
Jeremy Kent B Gross <jeremykentbgross@gmail.com>
2012-09-04 23:38:27 +0200

    Class Refector continue. 	modified:   notes/backlog.txt 	modified:   public/scripts/engine/Game2DPoint.js

--- public/scripts/engine/Game2DPoint.js
+++ public/scripts/engine/Game2DPoint.js
@@ -19,69 +19,76 @@
 	along with EmpathicCivGameEngine™.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+//TODO deprecated
 GameEngineLib.createGame2DPoint = function(inX, inY)
 {
-	var outPoint = new Object;
-	
-	outPoint.myX = inX || 0;
-	outPoint.myY = inY || 0;
-	
-	outPoint.add = function(inOther)
-	{
-		return GameEngineLib.createGame2DPoint(this.myX + inOther.myX, this.myY + inOther.myY);
-	}
-	
-	outPoint.subtract = function(inOther)
-	{
-		return GameEngineLib.createGame2DPoint(this.myX - inOther.myX, this.myY - inOther.myY);
-	}
-	
-	outPoint.multiply = function(inScalar)
-	{
-		return GameEngineLib.createGame2DPoint(this.myX * inScalar, this.myY * inScalar);
-	}
-	
-	outPoint.componentMax = function(inOther)
-	{
-		return GameEngineLib.createGame2DPoint(
-			Math.max(this.myX, inOther.myX),
-			Math.max(this.myY, inOther.myY)
-		);
-	}
-	outPoint.componentMin = function(inOther)
-	{
-		return GameEngineLib.createGame2DPoint(
-			Math.min(this.myX, inOther.myX),
-			Math.min(this.myY, inOther.myY)
-		);
-	}
-	
-	outPoint.lenSq = function()
-	{
-		return this.myX * this.myX + this.myY * this.myY;
-	}
-	
-	outPoint.len = function()
-	{
-		return Math.sqrt(this.myX * this.myX + this.myY * this.myY);
-	}
-	
-	outPoint.unit = function()
-	{
-		var len = Math.sqrt(this.myX * this.myX + this.myY * this.myY);
-		return GameEngineLib.createGame2DPoint(this.myX / len, this.myY / len);
-	}
-	
-	outPoint.dot = function(inOther)
-	{
-		return this.myX * inOther.myX + this.myY * inOther.myY;
-	}
-	
-	outPoint.floor = function()
-	{
-		return GameEngineLib.createGame2DPoint(Math.floor(this.myX), Math.floor(this.myY));
-	}
-	
-	return outPoint;
+	return new GameEngineLib.Game2DPoint(inX, inY);
 }
 
+
+
+
+GameEngineLib.Game2DPoint = function Game2DPoint(inX, inY)
+{
+	this.myX = inX || 0;
+	this.myY = inY || 0;
+}
+GameEngineLib.Game2DPoint.prototype.constructor = GameEngineLib.Game2DPoint;
+
+
+GameEngineLib.Game2DPoint.prototype.add = function add(inOther)
+{
+	return new GameEngineLib.Game2DPoint(this.myX + inOther.myX, this.myY + inOther.myY);
+}
+
+GameEngineLib.Game2DPoint.prototype.subtract = function subtract(inOther)
+{
+	return new GameEngineLib.Game2DPoint(this.myX - inOther.myX, this.myY - inOther.myY);
+}
+
+GameEngineLib.Game2DPoint.prototype.multiply = function multiply(inScalar)
+{
+	return new GameEngineLib.Game2DPoint(this.myX * inScalar, this.myY * inScalar);
+}
+
+GameEngineLib.Game2DPoint.prototype.componentMax = function componentMax(inOther)
+{
+	return new GameEngineLib.Game2DPoint(
+		Math.max(this.myX, inOther.myX),
+		Math.max(this.myY, inOther.myY)
+	);
+}
+GameEngineLib.Game2DPoint.prototype.componentMin = function componentMin(inOther)
+{
+	return new GameEngineLib.Game2DPoint(
+		Math.min(this.myX, inOther.myX),
+		Math.min(this.myY, inOther.myY)
+	);
+}
+
+GameEngineLib.Game2DPoint.prototype.lenSq = function lenSq()
+{
+	return this.myX * this.myX + this.myY * this.myY;
+}
+
+GameEngineLib.Game2DPoint.prototype.len = function len()
+{
+	return Math.sqrt(this.myX * this.myX + this.myY * this.myY);
+}
+
+GameEngineLib.Game2DPoint.prototype.unit = function unit()
+{
+	var len = Math.sqrt(this.myX * this.myX + this.myY * this.myY);
+	return new GameEngineLib.Game2DPoint(this.myX / len, this.myY / len);
+}
+
+GameEngineLib.Game2DPoint.prototype.dot = function dot(inOther)
+{
+	return this.myX * inOther.myX + this.myY * inOther.myY;
+}
+
+GameEngineLib.Game2DPoint.prototype.floor = function floor()
+{
+	return new GameEngineLib.Game2DPoint(Math.floor(this.myX), Math.floor(this.myY));
+}
